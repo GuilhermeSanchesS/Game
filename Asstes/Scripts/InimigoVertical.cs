b2f8a7a33693b914f8e9fcf6367a286480350282
GuilhermeSanchesS <62891985+GuilhermeSanchesS@users.noreply.github.com>
2020-07-21 14:53:23 -0300

    Add files via upload

---
+++ Asstes/Scripts/InimigoVertical.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class InimigoVertical : MonoBehaviour
+{
+    private float time = 0.0f;
+
+    public float timer;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= timer)
+        {
+            time = 0f;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+        }
+    }
+}
